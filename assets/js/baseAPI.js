f7654a5f374e03db074025898455e398c2719941
zyl_sunshine <zhouyl7887@163.com>
2020-09-13 10:37:08 +0800

    完成了登录和注册功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,5 @@
+//每次调用$.get() $.post()  $.ajax()的时候,会先调用ajaxPrefilter这个函数
+// 在这个函数中 可以拿到我们给ajax提供的配置对象 
 $.ajaxPrefilter(function(options){
     options.url='http://ajax.frontend.itheima.net'+ options.url;
 })
